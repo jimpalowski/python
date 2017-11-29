58ca7c35e97007f008cd7a798f5a358ab57d13fb
Jim Palowski <jimmy@Jims-MacBook-Pro.local>
2017-11-28 22:18:57 -0800

    _100.py

--- _100.py
+++ _100.py
@@ -1,2 +1,8 @@
-for i in range(0,101,2):
-    print(i)
+def one_to_100():
+    for i in range(0,101,2):
+        print(i)
+
+
+one_to_100()
+
+# Make an algorithm printing the even numbers from 1 to 100#
