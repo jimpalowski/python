f538991ff06c8b81d57033f09bdae9e3a071d287
Jim Palowski <jimmy@Jims-MacBook-Pro.local>
2017-11-29 20:51:10 -0800

    fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz():
+    for n in range(1,11):
+        if n % 2 == 0:
+            print("fizz")
+        else:
+            print("buzz")
+
+fizz_buzz()
+
+#Uploading fizz_buzz.py##
+
