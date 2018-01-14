d5219cc139e1d3bcf87d5282f208de91fb974412
Jim Palowski <33942859+jimpalowski@users.noreply.github.com>
2018-01-13 19:39:40 -0800

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,46 @@
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+
+def are(list_one, list_two):
+    if list_one == list_two:
+        print "The lists are the same."
+    else:
+        print "The lists are not the same."
+        
+      
+are(list_one, list_two)
+
+
+
+list_one = [1,2,5,6,5]
+list_two = [1,2,5,6,5,3]
+def mx(list_one, list_two):
+        if list_one == list_two:
+            print "The lists are the same."
+        else:
+            print "The lists are not the same."
+mx(list_one, list_two)  
+
+
+list_one = [1,2,5,6,5,16]
+list_two = [1,2,5,6,5]
+
+def ti(list_one, list_two):
+    if list_one == list_two:
+        print "The lists are the same."
+    else:
+        print "The lists are not the same."
+ti(list_one, list_two)      
+
+
+
+list_one = ['celery', 'carrots', 'bread', 'milk']
+list_two = ['celery','carrots','bread','cream']
+
+def xi(list_one, list_two):
+    if list_one == list_two:
+        print "The lists are the same."
+    else:
+        print "The lists are not the same."
+xi(list_one, list_two)      
+
