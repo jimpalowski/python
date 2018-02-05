e258dcc3c4d225ef91f777fa0ad34181b65cd9b0
Jim Palowski <33942859+jimpalowski@users.noreply.github.com>
2018-02-04 23:58:15 -0800

    Bubble.py

---
+++ bubble.py
@@ -0,0 +1,23 @@
+def bubble(list):
+    list_len = (list)
+    for x in range(list_len)
+        swapped = False
+
+
+
+        for y in range(0, list_len-x-1):
+            if list[y] > list[y+1]
+                list[y], list[y+1] = list[y+1], list[y]
+
+
+                print("swap needed")
+                swapped = True
+
+            if swapped == False:
+                print("No swap need")
+                break   
+    return list
+list1 = [1,52,73,12,75,221,121]
+list2 = [11,32,64,12,32,634,12]
+print(bubble(list))
+
