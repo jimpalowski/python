8d1843ccf7a2af7aa7e10da960af57a4f453a68f
Jim Palowski <33942859+jimpalowski@users.noreply.github.com>
2018-02-07 15:38:13 -0800

    LinearSearch.py

---
+++ linearsearch.py
@@ -0,0 +1,10 @@
+def search(list,x):
+    for x in range(len(list)):
+        if list[i] == x:
+            return x
+
+
+    return "Not found"
+
+list = [43,12,53,12,75,21]
+print(search(list, 9))            
